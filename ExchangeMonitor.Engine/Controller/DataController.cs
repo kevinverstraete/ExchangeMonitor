fcfaba94a11912ccad78e03350f30bc9c16beae5
kevinverstraete <verstraete_kevin@telenet.be>
2015-07-26 17:04:15 +0200

    rework

--- ExchangeMonitor.Engine/Controller/DataController.cs
+++ ExchangeMonitor.Engine/Controller/DataController.cs
@@ -5,42 +5,76 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Timers;
 
-namespace ExchangeMonitor.Engine.Controller {
-  public class DataController {
+namespace ExchangeMonitor.Engine.Controller
+{
+    public class DataController
+    {
+        #region private members
+        private Dictionary<string, DataControllerEventArgs> _tickers = new Dictionary<string, DataControllerEventArgs>();
+        private Timer _timer;
+        #endregion private members
 
-    private List<string> _tickers = new List<string>();
-    private Timer _timer = new Timer(5000);
+        #region events
+        public event EventHandler DataFetched;
+        protected virtual void OnDataFetched(EventArgs e)
+        {
+            if (DataFetched != null) DataFetched(this, e);
+            DataFetched += DataControllerDataFetched;
+        }
+        #endregion events
 
-    public event EventHandler DataFetched;
-    protected virtual void OnDataFetched(EventArgs e) {
-      if (DataFetched != null) DataFetched(this, e);
+        #region constructors
+        public DataController(): this(5000) { }
+        public DataController(double interval)
+        {
+            _timer = new Timer(interval);
+            _timer.Elapsed += _onTimerElapsed;
+            _timer.Enabled = true;
+        }
+        #endregion constructors
+
+        #region Timer
+        private void _onTimerElapsed(object sender, ElapsedEventArgs e)
+        {
+            foreach (var item in _tickers)
+            {
+                var webData = Web.DataCatcher.Catch(item.Key);
+                var webInfo = Web.InfoCatcher.Catch(item.Key);
+                var data = new ViewModel.Data()
+                {
+                    Ticker = item.Key,
+                    StockExchange = webInfo.StockExchange,
+                    Name = webData.Name,
+                    Rate = webData.Rate,
+                };
+                OnDataFetched(new DataControllerEventArgs() { Data = data, Success = webData.Success });
+            }
+        }
+        #endregion Timer
+
+        #region Alarm
+        private void DataControllerDataFetched(object sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+        #endregion Alarm
+
+        #region Tickers
+        public void AddTickers(string ticker)
+        {
+            _tickers.Add(ticker, new DataControllerEventArgs());
+        }
+
+        public void RemoveTicker(string ticker)
+        {
+            _tickers.Remove(ticker);
+        }
+        #endregion Tickers
     }
 
-    public DataController() {
-      _timer.Elapsed += _timerElapsed;
-      _timer.Enabled = true;
+    public class DataControllerEventArgs : EventArgs
+    {
+        public ViewModel.Data Data { get; set; }
+        public bool Success { get; set; }
     }
-
-    private void _timerElapsed(object sender, ElapsedEventArgs e) {
-      foreach (var item in _tickers) {
-        var webData = Web.DataCatcher.Catch(item);
-        var data = new ViewModel.Data() {
-          Ticker = item,
-          Name = webData.Name,
-          Rate = webData.Rate
-        };
-        OnDataFetched(new DataControllerEventArgs() { Data = data, Success = webData.Success });
-      }
-    }
-
-    public void AddTickers(string ticker){
-      if (!_tickers.Contains(ticker)) 
-        _tickers.Add(ticker);
-    }
-  }
-
-  public class DataControllerEventArgs : EventArgs {
-    public ViewModel.Data Data { get; set; }
-    public bool Success { get; set; }
-  }
 }
--- ExchangeMonitor.Engine/ViewModel/Data.cs
+++ ExchangeMonitor.Engine/ViewModel/Data.cs
@@ -8,16 +8,19 @@ namespace ExchangeMonitor.Engine.ViewModel {
   public class Data {
     public string Ticker { get; set; }
     public string Name { get; set; }
+    public string StockExchange { get; set; }
     public double Rate { get; set; }
     public double BollingerUpper { get; set; }
     public double BollingerLower { get; set; }
 
-    public override string ToString() {
-      return "Ticker: " + Ticker
-          + "\nName: " + Name
-          + "\nRate: " + Rate.ToString()
-          + "\nBollingerUpper: " + BollingerUpper.ToString()
-          + "\nBollingerLower: " + BollingerLower.ToString();
+    public override string ToString()
+    {
+        return "Ticker: " + Ticker
+            + "\nName: " + Name
+            + "\nStockExchange: " + StockExchange
+            + "\nRate: " + Rate.ToString()
+            + "\nBollingerUpper: " + BollingerUpper.ToString()
+            + "\nBollingerLower: " + BollingerLower.ToString();
     }
   }
 }
